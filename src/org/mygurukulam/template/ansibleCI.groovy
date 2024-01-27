01239e126dd691f5fcff9db2dcd4714173529444
shreya-0522 <150731531+shreya-0522@users.noreply.github.com>
2024-01-27 13:59:19 +0530

    Update ansibleCI.groovy

--- src/org/mygurukulam/template/ansibleCI.groovy
+++ src/org/mygurukulam/template/ansibleCI.groovy
@@ -4,13 +4,16 @@ import org.mygurukulam.common.*
 import org.mygurukulam.ansible.*
 
 def call(String name , Map config = [:]) {
-  //def git = new org.mygurukulam.common.Checkout()
 //def play = new org.mygurukulam.ansible.Playbook()
 def hi = new org.mygurukulam.common.Hello()
 
-//git.call(url, creds, branch)
 //play.runAnsiblePlaybook(inventoryPath, testPlaybookPath)
 hi.hello(name)
+}
+
+def checkout( String url ,String creds ,String branch , Map config = [:] ) {
+   def git = new org.mygurukulam.common.Checkout()
+    git.call(url, creds, branch)
 }
 
 
